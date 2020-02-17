9a2ac4b6cccfa04011670053ec4f6fe83a0259d0
maks45 <maksim.durov45@gmail.com>
2020-02-17 17:50:50 +0200

    init commit

---
+++ app/src/main/java/com/maksim/durov/simplenotes/MainActivity.java
@@ -0,0 +1,14 @@
+package com.maksim.durov.simplenotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
